94526d470de52b6473166de4040eaa11fbbb8409
Olav Fosse <fosseolav@gmail.com>
2020-07-04 23:39:17 +0200

    Remove inline radio

--- config.el
+++ config.el
@@ -62,43 +62,10 @@
   (call-interactively 'counsel-find-file-extern)
   (setq default-directory temp))
 
+(map! :leader (:prefix ("o" . "open") :desc "Open a book" "B" 'olav-open-book))
+
 (setq user-full-name "Olav Fosse"
       user-mail-address "fosseolav@gmail.com")
-
-; should be defvar when packaged
-(setq er-channels '(("def con | soma fm" . "http://somafm.com/defcon256.pls")
-                    ("cyberia | lainon". "http://lainon.life:8000/cyberia.ogg")
-                    ("cafe | lainon" . "http://lainon.life:8000/cafe.ogg")
-                    ("swing | lainon" . "http://lainon.life:8000/swing.ogg")
-                    ("everything | lainon" . "http://lainon.life:8000/everything.ogg")
-                    ("metal | soma fm" . "http://somafm.com/metal130.pls")
-                    ("groove salad | soma fm" . "http://somafm.com/groovesalad256.pls")
-                    ("secret agent | soma fm" . "http://www.somafm.com/secretagent.pls")
-                    ("ryno the bearded" . "http://stream.ryno.cc/oo")
-                    ("jamendo lounge" . "http://streaming.radionomy.com/JamendoLounge") ; Great music, but the songs repeat themselfs frequently
-                    ))
-
-(defun er-alist-keys (alist) (mapcar 'car alist))
-
-(defun er-stop () (interactive) (when (boundp 'radio-process) (delete-process radio-process)))
-
-(defun er-play-low-level (url) (setq radio-process (start-process "emacs-radio" nil "vlc" "--no-video" "-I" "rc" url)))
-
-(defun er-get-url ()
-  (setq er-channel (completing-read
-                       "Channel: "
-                       (er-alist-keys er-channels)
-                       nil nil))
-  (or (cdr (assoc er-channel er-channels)) er-channel))
-
-(defun er-play ()
-  (interactive)
-  (setq url (er-get-url))
-  (er-stop)
-  (er-play-low-level url))
-
-(map! :leader (:prefix ("r" . "radio") :desc "Play radio" "p" 'er-play))
-(map! :leader (:prefix ("r" . "radio") :desc "Stop radio" "s" 'er-stop))
 
 (setq projectile-project-search-path '("~/code/"))
 
